4bd559ad9ebf30516adbd29d402f1bfe07d5b258
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-06-09 11:45:51 +0200

    Solved lab

--- testing-intro/spec/fizzBuzz.spec.js
+++ testing-intro/spec/fizzBuzz.spec.js
@@ -4,3 +4,29 @@
 // divisible by 3 and 5 -> 'fizzbuzz'
 // in all other cases you just say the number
 // write a function that gets a number as a parameter and returns the correct result
+
+const fizzBuzz = require('../src/fizzBuzz')
+
+describe('fizzbuzz', function() {
+    it('it is a function', function() {
+        expect(typeof fizzBuzz).toEqual('function')
+    })
+    it('returns 1 for 1', function() {
+        expect(fizzBuzz(1)).toEqual(1)
+    })
+    it('returns 2 for 2', function() {
+        expect(fizzBuzz(2)).toEqual(2)
+    })
+    it('returns fizz for 3', function() {
+        expect(fizzBuzz(3)).toEqual('fizz')
+    })
+    it('returns fizz for 6', function() {
+        expect(fizzBuzz(6)).toEqual('fizz')
+    })
+    it('returns buzz for 5', function() {
+        expect(fizzBuzz(5)).toEqual('buzz')
+    }) 
+    it('returns fizzbuzz for 15', function () {
+		expect(fizzBuzz(15)).toEqual('fizzbuzz')
+	})
+})
--- testing-intro/spec/greeting.spec.js
+++ testing-intro/spec/greeting.spec.js
@@ -1,3 +1,15 @@
 // this imports the greeting function
 
-const greeting = require('../src/greeting')
+const greeting = require('../src/greeting') 
+// terminology: 
+// a test suite 
+describe('The function greeting', function() {
+    // spec - specification
+    it('it should be a function', function() {
+        // expectation - using a matcher 
+        expect(typeof greeting).toBe('function')
+    }); 
+    it('should return a greeting', function() {
+        expect(greeting()).toEqual('hello ironhackers!')
+    })
+})
